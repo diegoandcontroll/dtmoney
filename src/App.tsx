f6f54bbb229687fa994c05f3c76b9211e93083f1
diegoandcontroll <lukasxdp2@gmail.com>
2022-05-18 20:08:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { GlobalStyle } from './styles/global';
+import {Header} from './components/Header';
+import { Dashboard } from './components/Dashboard';
+function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,5 @@
+export function Summary() {
+  return (
+    <div>index</div>
+  )
+}
