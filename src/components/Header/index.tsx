28672b05a0a2cd5768946e24c70a3aff9fae48f6
diegoandcontroll <lukasxdp2@gmail.com>
2022-05-19 11:28:02 -0300

    feat: finish modal and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg';
 import {Container, Content} from './styles';
-export  function Header() {
+interface HeaderProps {
+  toggleModal: () => void;
+}
+export  function Header({toggleModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="logo" />
-        <button type='button'>Nova transação</button>
+        <button type='button' onClick={toggleModal}>Nova transação</button>      
       </Content>
     </Container>
   )
 }
+ 
