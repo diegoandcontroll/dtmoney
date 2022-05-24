26c69116b3d58f123da04c5e9347d8b7b4d372f8
diegoandcontroll <lukasxdp2@gmail.com>
2022-05-24 19:28:32 -0300

    feat: create hook usetransactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import { GlobalStyle } from './styles/global';
-import {Header} from './components/Header';
-
-import { Dashboard } from './components/Dashboard';
-import { TransactionModal } from './components/NewTransactionModal';
+import React, { useState } from "react";
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { TransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 function App() {
   const [modal, setModal] = useState(false);
 
-  function toggleModal(){
+  function toggleModal() {
     setModal(!modal);
   }
   return (
-    <>
-      <Header toggleModal={toggleModal}/>
-
+    <TransactionsProvider>
+      <Header toggleModal={toggleModal} />
       <Dashboard />
-      <TransactionModal isOpen={modal} onRequestClose={toggleModal}/>
+      <TransactionModal isOpen={modal} onRequestClose={toggleModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,29 @@
 import { Container } from "./styles";
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
-import totalImg from '../../assets/total.svg';
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../hooks/useTransactions";
+
 export function Summary() {
+  const { transactions } = useTransactions();
+
+  const sumary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposit += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
   return (
     <Container>
       <div>
@@ -10,14 +31,26 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="" />
         </header>
-        <strong>R$300,00</strong>
+        <strong>
+          {" "}
+          {new Intl.NumberFormat("pt-BR", {
+            currency: "BRL",
+            style: "currency",
+          }).format(sumary.deposit)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="" />
         </header>
-        <strong>- R$60,00</strong>
+        <strong>
+          {"- "}
+          {new Intl.NumberFormat("pt-BR", {
+            currency: "BRL",
+            style: "currency",
+          }).format(sumary.withdraw)}
+        </strong>
       </div>
 
       <div className="highlight-bg">
@@ -25,8 +58,13 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt="" />
         </header>
-        <strong>R$240,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            currency: "BRL",
+            style: "currency",
+          }).format(sumary.total)}
+        </strong>
       </div>
     </Container>
-  )
+  );
 }
